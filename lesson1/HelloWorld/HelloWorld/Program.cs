ab9dae665d765ab587e8e692638d3e12d2541c51
IlyaIvanov <ilyaivanov7567@gmail.com>
2021-08-23 23:36:07 +0300

    HelloWorld

---
+++ lesson1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What`s your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}");
+            Console.ReadKey();
+
+        }
+    }
+}
