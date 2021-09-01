b64d0a3393f83a7b55b1330472abace341d864d3
IlyaIvanov <ilyaivanov7567@gmail.com>
2021-09-01 23:57:03 +0300

    diagonal refactoring

--- lesson3/1 diagonal/diagonal/diagonal/Program.cs
+++ lesson3/1 diagonal/diagonal/diagonal/Program.cs
@@ -11,10 +11,30 @@ namespace diagonal
         static void Main(string[] args)
         {
             int[,] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            Console.WriteLine("Исходный массив:");
             for (int i = 0; i < arr.GetLength(0); i++)
             {
-                Console.SetCursorPosition(3*i, i);
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Элементы диагонали 1:");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                Console.SetCursorPosition(i, i + 5);
                 Console.WriteLine("{0}", arr[i, i]);
+            }
+
+            Console.WriteLine("Элементы диагонали 2:");
+
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                Console.SetCursorPosition(arr.GetLength(0)-i-1, i + 9);
+                Console.WriteLine("{0}", arr[i, arr.GetLength(0) - i - 1]);
             }
             Console.ReadKey();
         }
