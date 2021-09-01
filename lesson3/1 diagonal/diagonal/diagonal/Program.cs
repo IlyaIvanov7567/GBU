f69393a57a552d386ab983ec73ce8f9e27eea698
IlyaIvanov <ilyaivanov7567@gmail.com>
2021-09-01 23:21:28 +0300

    diagonal refactoring

--- lesson3/1 diagonal/diagonal/diagonal/Program.cs
+++ lesson3/1 diagonal/diagonal/diagonal/Program.cs
@@ -13,7 +13,8 @@ namespace diagonal
             int[,] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
             for (int i = 0; i < arr.GetLength(0); i++)
             {
-                System.Console.WriteLine("{0}", arr[i, i]);
+                Console.SetCursorPosition(3*i, i);
+                Console.WriteLine("{0}", arr[i, i]);
             }
             Console.ReadKey();
         }
