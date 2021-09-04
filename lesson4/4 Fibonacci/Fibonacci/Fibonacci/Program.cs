1d5df620866d67fe4e80a54f46691c921d41c033
IlyaIvanov <ilyaivanov7567@gmail.com>
2021-09-04 16:39:58 +0300

    Fibonacci resolved

---
+++ lesson4/4 Fibonacci/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonacci(10));
+            Console.ReadKey();
+        }
+        static int Fibonacci(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
