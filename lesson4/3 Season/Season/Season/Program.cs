44b95bc85a852a793902e9a1953cddc8af4a38cc
IlyaIvanov <ilyaivanov7567@gmail.com>
2021-09-04 16:38:39 +0300

    Season resolved

---
+++ lesson4/3 Season/Season/Season/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        enum Season
+        {
+            error,
+            Winter,
+            Spring,
+            Summer,
+            Autumn
+        }
+        static void Main(string[] args)
+        {
+            inputmess();
+            while (true)
+            {
+                bool success = Int32.TryParse(Console.ReadLine(), out int month_number);
+                if (success)
+                {
+                    Console.WriteLine(TranslateSeason(GetSeason(month_number)));
+                    inputmess();
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: введите число от 1 до 12 и нажмите Enter");
+                }
+            }
+        }
+        static void inputmess()
+        {
+            Console.WriteLine("Введите порядковый номеря месяца и нажмите Enter");
+        }
+        static Season GetSeason(int value)
+        {
+            switch (value)
+            {
+                case 1:
+                case 2:
+                case 12:
+                    return Season.Winter;
+                case 3:
+                case 4:
+                case 5:
+                    return Season.Spring;
+                case 6:
+                case 7:
+                case 8:
+                    return Season.Summer;
+                case 9:
+                case 10:
+                case 11:
+                    return Season.Autumn;
+                default:
+                    return Season.error;
+            }
+        }
+        static string TranslateSeason(Season value)
+        {
+            string result;
+            switch (value)
+            {
+                case Season.Winter:
+                    result = "Зима";
+                    break;
+                case Season.Spring:
+                    result = "Весна";
+                    break;
+                case Season.Summer:
+                    result = "Лето";
+                    break;
+                case Season.Autumn:
+                    result = "Осень";
+                    break;
+                default:
+                    result = "Ошибка";
+                    break;
+            }
+            return result;
+        }
+    }
+}
