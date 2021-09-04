f0b7ad17391ec0a0cb72956d136eac07029d5368
IlyaIvanov <ilyaivanov7567@gmail.com>
2021-09-04 16:34:02 +0300

    GetFullName resolved

---
+++ lesson4/1 GetFullName/GetFullName/GetFullName/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstNameList = { "Илья", "Кирилл", "Денис", "Михаил", "Артем" };
+            string[] lastNameList = { "Иванов", "Сидоров", "Вольнов", "Фокин", "Агаян" };
+            string[] patronymicList = { "Евгеньевич", "Сергеевич", "Евгеньевич", "Вячеславовчи", "Карапетович" };
+            OutputFullName(firstNameList, lastNameList, patronymicList);
+            Console.ReadKey();
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            string FullName = $"{lastName} {firstName} {patronymic}";
+            return FullName;
+        }
+
+        static void OutputFullName(string[] firstNameList, string[] lastNameList, string[] patronymicList)
+        {
+            for (int i = 0; i < firstNameList.Length; i++)
+            {
+                Console.WriteLine($"{GetFullName(firstNameList[i], lastNameList[i], patronymicList[i])}");
+            }
+        }
+    }
+}
