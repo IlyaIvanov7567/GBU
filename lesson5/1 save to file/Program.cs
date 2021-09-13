7c59480d3a3a75f0d3ff14ce47aa86e630bc6eb7
IlyaIvanov <ilyaivanov7567@gmail.com>
2021-09-13 05:44:42 +0300

    lesson5 resoslved

---
+++ lesson5/1 save to file/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace save_to_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "textfile.txt";
+
+            Console.WriteLine("Введите произвольный набор данных:");
+
+            string data = Console.ReadLine();
+
+            File.AppendAllText(filename, data);
+
+            Console.WriteLine(File.ReadAllText("textfile.txt"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lesson5/directory tree wo recursion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace directory_tree_wo_recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\user\Desktop\root";
+            string[] allfolders = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+            string[] allfiles = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+
+            foreach (string folder in allfolders)
+            {
+                File.AppendAllText("result.txt", $"{folder} \n");
+                foreach (string file in allfiles)
+                {
+                    File.AppendAllText("result.txt", $"{file} \n");
+                }
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+
