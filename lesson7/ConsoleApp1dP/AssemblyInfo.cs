26d0d6b88a0b53e178e8a0f9d6673ffaba9db0ec
IlyaIvanov <ilyaivanov7567@gmail.com>
2021-09-16 07:21:43 +0300

    lesson7 resolved

---
+++ lesson7/ConsoleApp1dP/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("ConsoleApp1")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyVersion("1.0.0.0")]
